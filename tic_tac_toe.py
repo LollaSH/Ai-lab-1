0060b3df53fcc49e56449cf77458d70a1ea3390b
edmondmentori <edmond.mentori@gmail.com>
2025-02-08 16:47:17 +0100

    Can now put pieces correctly in horiz. and vert. plane
    Checks allowed moves in horizontal and vertical plane

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -169,6 +169,75 @@ def possible_moves(board, player):
 
     return moves
 
+def allowed_moves(board, player):
+    d = np.ones((8,8), dtype=int)
+    
+    for i in range(8):
+        for j in range(8):
+            if board[i, j] == player:
+                flips = 0
+                for hp in range(i+1, 8):
+                    b = board[hp, j]
+                    if b == -player:
+                        flips+=1
+                    elif b == 0 and flips != 0:
+                        d[hp, j] = 0
+                        flips = 0
+                        break
+                    elif b == player:
+                        flips = 0
+                        break
+                    elif b == 0 and flips == 0:
+                        break
+                flips = 0
+                for hn in reversed(range(0, i)):
+                    b = board[hn, j]
+                    if b == -player:
+                        flips+=1
+                    elif b == 0 and flips != 0:
+                        d[hn, j] = 0
+                        flips = 0
+                        break
+                    elif b == player:
+                        flips = 0
+                        break
+                    elif b == 0 and flips == 0:
+                        break
+                flips = 0
+                for vp in range(j + 1, 8):
+                    b = board[i, vp]
+                    if b == -player:
+                        flips+=1
+                    elif b == 0 and flips != 0:
+                        d[i, vp] = 0
+                        flips = 0
+                        break
+                    elif b == player:
+                        flips = 0
+                        break
+                    elif b == 0 and flips == 0:
+                        break
+                flips = 0
+                for vn in reversed(range(0, j)):
+                    b = board[i, vn]
+                    if b == -player:
+                        flips+=1
+                    elif b == 0 and flips != 0:
+                        d[i, vn] = 0
+                        flips = 0
+                        break
+                    elif b == player:
+                        flips = 0
+                        break
+                    elif b == 0 and flips == 0:
+                        break
+                
+    
+    
+    empty = list(zip(*np.where(d == 0)))
+
+    return empty
+
 def update_board(board, move):
     board = board.copy()
 
@@ -256,17 +325,12 @@ class HumanPlayer:
 
         board = env.get_board()
         pieces = set(zip(*np.where(board == self.player)))
-        empty = set(zip(*np.where(board == 0)))
+        empty = allowed_moves(board=board, player = self.player)
 
         try:
             move = {
                 'player': self.player
             }
-            if len(pieces) >= 3:
-                pos = self.get_click()
-                while pos not in pieces: pos = self.get_click()
-                move['take'] = pos
-
             pos = self.get_click()
             while pos not in empty: pos = self.get_click()
             move['put'] = pos
@@ -439,7 +503,7 @@ def main():
 
     graphics = Graphics()
     env = TicTacToe()
-    graphics.draw(board = env.get_board())
+    #graphics.draw(board = env.get_board())
 
     n_wins = {
         1: 0,
